07fa4529e07f129343eeba061ae90e7ace6ef9ca
Tom Logan <tom.logan@canterbury.ac.nz>
2021-06-07 20:12:53 +1200

    adding histogram and cdf

--- src/data_export.py
+++ src/data_export.py
@@ -4,6 +4,7 @@ Export data for the d3 app
 import yaml
 import main
 import pandas as pd
+import numpy as np
 
 config_filename = 'main'
 # import config file
@@ -18,9 +19,9 @@ db = main.init_db(config)
 
 
 # distance as csv - only nonzero pop
-sql = "SELECT geoid, dest_type, distance as lat FROM nearest_block WHERE population > 0"
+sql = "SELECT geoid, dest_type, distance FROM nearest_block WHERE population > 0"
 dist = pd.read_sql(sql, db['con'])
-dist.to_csv('data/results/distances.csv')
+dist.to_csv('./data/results/distances.csv')
 
 
 # topojson
@@ -31,5 +32,30 @@ dist.to_csv('data/results/distances.csv')
 # destinations: dest_type, lat, lon
 sql = "SELECT dest_type, st_x(geom) as lon, st_y(geom) as lat FROM destinations"
 dist = pd.read_sql(sql, db['con'])
-dist.to_csv('data/results/destinations.csv')
+dist.to_csv('./data/results/destinations.csv')
 
+###
+# histogram and cdf
+###
+# import data
+sql = "SELECT geoid, dest_type, distance, population FROM nearest_block WHERE population > 0"
+df = pd.read_sql(sql, db['con'])
+# set bins
+bins = list(range(0,21))
+# create hist and cdf
+hists = []
+for service in df.dest_type.unique():
+    df_sub = df[df.dest_type==service]
+    # create the hist
+    count, division = np.histogram(df_sub.distance/1000, bins = bins, weights=df_sub.population, density=True)
+    count = np.append(0, count)
+    division = np.append(0, division)
+    df_new = pd.DataFrame({'pop_perc':count, 'distance':division[:-1]})
+    df_new['service']=service
+    df_new['pop_perc'] = df_new['pop_perc']*100
+    df_new['pop_perc_cum'] = df_new.pop_perc.cumsum()
+    hists.append(df_new)
+# concat
+df_hists = pd.concat(hists)
+# export
+df_hists.to_csv('./data/results/access_histogram.csv')
